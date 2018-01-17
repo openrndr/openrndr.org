01b868c56968d0948c522cf9004e5446e259246e
Gábor Kerekes <gbr@krks.info>
2018-01-17 11:27:41 +0100

    dump all dato data to data/dato-dump.json

--- data/fetch.ts
+++ data/fetch.ts
@@ -10,19 +10,48 @@ interface LoadResult {
   showcase: { toMap: () => Showcase };
 }
 
+function dump(repo: any) {
+  const filepath = path.resolve(__dirname, "dato-dump.json");
+  console.log("dumping repo to", filepath);
+  const res: any = {};
+  [
+    "calendar",
+    "about",
+    "community",
+    "gettingStarted",
+    "landing",
+    // "projects",
+    "showcase"
+  ].forEach(key => {
+    res[key] = repo[key].toMap();
+  });
+  fs.writeFileSync(filepath, JSON.stringify(res, null, 4));
+}
+
 (async function load() {
   await loader.load();
-  const result: LoadResult = loader.itemsRepo;
-  const showcase = result.showcase.toMap();
+  const result: any = loader.itemsRepo;
+  dump(result);
+
+  const {
+    calendar,
+    about,
+    community,
+    gettingStarted,
+    landing,
+    projects,
+    showcase
+  } = result;
+
   const paginatedGallery = paginate(
-    multiple(showcase.gallery),
+    multiple(showcase.toMap().gallery),
     index => `/showcase/gallery-${index}.json`,
     4
   );
 
   paginatedGallery.forEach((page, i) => {
     const filename = path.resolve("public", "showcase", `gallery-${i}.json`);
-    console.log("should save to", filename);
+    // console.log("should save to", filename);
     fs.writeFileSync(filename, JSON.stringify(page, null, 4));
   });
 })();
