2965d13dfee2e640a751d873c2b94528e36ae406
Gábor Kerekes <gbr@krks.info>
2018-01-17 16:01:56 +0100

    Merge home.tsx

--- src/app/containers/home.tsx
+++ src/app/containers/home.tsx
@@ -1,18 +1,43 @@
 import React from "react";
-import {prefetch} from "react-static";
-import {Location} from "history";
-
-import Section from '../components/section/index';
-import SectionHeader from '../components/section/section-header';
-import SectionBody from '../components/section/section-body';
+import { prefetch } from "react-static";
+import { Location } from "history";
+import Section from "../components/section/index";
+import SectionHeader from "../components/section/section-header";
+import SectionBody from "../components/section/section-body";
+import {
+  Event,
+  Calendar,
+  About,
+  Community,
+  GettingStarted,
+  Landing,
+  Paged,
+  Project
+} from "../../types";
 
 interface State {
   isInitialDataFetched: boolean;
   data: any;
 }
 
-interface Props {
+export interface DataProps {
+  calendar: {
+    events: Paged<Event>;
+  };
+  about: About;
+  community: Community;
+  gettingStarted: GettingStarted;
+  landing: Landing;
+  showcase: {
+    gallery: Paged<Project>;
+    experiments: Paged<Project>;
+    caseStudies: Paged<Project>;
+  };
+}
+
+export interface Props {
   location: Location;
+  data: DataProps;
 }
 
 export default class Home extends React.Component<Props, State> {
@@ -22,13 +47,12 @@ export default class Home extends React.Component<Props, State> {
   };
 
   componentWillMount() {
-    prefetch("/data")
-        .then(({initialProps}: any) => {
-          this.setState({
-            isInitialDataFetched: true,
-            data: initialProps
-          })
-        })
+    prefetch("/data").then(({ initialProps }: any) => {
+      this.setState({
+        isInitialDataFetched: true,
+        data: initialProps
+      });
+    });
   }
 
   render() {
@@ -38,25 +62,17 @@ export default class Home extends React.Component<Props, State> {
     console.log(this.props.location.pathname);
 
     return (
-        <div className="sweet-home">
-          <Section name={"intro"}>
-            <SectionHeader>
-              here will be the intro VIDEO/IMAGE
-            </SectionHeader>
-            <SectionBody name={"Landing"}>
-              WHATEVER CONTENT IS
-            </SectionBody>
-          </Section>
+      <div className="sweet-home">
+        <Section name={"intro"}>
+          <SectionHeader>here will be the intro VIDEO/IMAGE</SectionHeader>
+          <SectionBody name={"Landing"}>WHATEVER CONTENT IS</SectionBody>
+        </Section>
 
-          <Section name={"getting-started"}>
-            <SectionHeader>
-              Getting started
-            </SectionHeader>
-            <SectionBody name={"Showcase"}>
-              WHATEVER CONTENT IS
-            </SectionBody>
-          </Section>
-        </div>
-    )
+        <Section name={"getting-started"}>
+          <SectionHeader>Getting started</SectionHeader>
+          <SectionBody name={"Showcase"}>WHATEVER CONTENT IS</SectionBody>
+        </Section>
+      </div>
+    );
   }
 }
