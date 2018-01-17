cef3c1f5481a1a0dea864cae2c3b893cecf7202f
boydrotgans <boyd@lustlab.net>
2018-01-17 16:50:00 +0100

    home merge

--- src/app/containers/home.tsx
+++ src/app/containers/home.tsx
@@ -60,21 +60,61 @@ export default class Home extends React.Component<Props, State> {
   render() {
     if (!this.state.data) return null;
 
-    //use the pathname to set the scroll position
-    // console.log(this.props.location.pathname);
-    console.log("HERE IS SOME DATA ", this.state.data.calendar.events);
-    return (
-      <div className="sweet-home">
-        <Section name={"intro"}>
-          <SectionHeader>here will be the intro VIDEO/IMAGE</SectionHeader>
-          <SectionBody name={"Landing"}>WHATEVER CONTENT IS</SectionBody>
-        </Section>
+    // use the pathname to set the scroll position
+    console.log(this.props.location.pathname);
 
-        <Section name={"getting-started"}>
-          <SectionHeader>Getting started</SectionHeader>
-          <SectionBody name={"Showcase"}>Some showcased content</SectionBody>
-        </Section>
-      </div>
-    );
+    return (
+        <div className="sweet-home">
+          <Section name={"intro"}>
+            <SectionHeader>
+              <iframe src="https://player.vimeo.com/video/97314422?loop=1&title=0&byline=0&portrait=0" width="100%" height="300px"></iframe>
+            </SectionHeader>
+            <SectionBody name={"Landing"}>
+              WHATEVER CONTENT IS
+            </SectionBody>
+          </Section>
+
+          <Section name={"getting-started"}>
+            <SectionHeader>
+                <h1>Getting started</h1> <a href="https://github.com/">Github</a>
+            </SectionHeader>
+            <SectionBody name={"Getting Started"}>
+              WHATEVER CONTENT IS
+            </SectionBody>
+          </Section>
+          <Section name={"Showcase"}>
+            <SectionHeader>
+               <h1>Showcase</h1>
+            </SectionHeader>
+            <SectionBody name={"Showcase"}>
+                WHATEVER CONTENT IS
+            </SectionBody>
+          </Section>
+          <Section name={"Community"}>
+            <SectionHeader>
+                <h2>Community</h2>
+            </SectionHeader>
+            <SectionBody name={"Community"}>
+                WHATEVER CONTENT IS
+            </SectionBody>
+          </Section>
+            <Section name={"About"}>
+                <SectionHeader>
+                    <h2>About</h2>
+                </SectionHeader>
+                <SectionBody name={"About"}>
+                    WHATEVER CONTENT IS
+                </SectionBody>
+            </Section>
+            <Section name={"Calender"}>
+                <SectionHeader>
+                    <h2>Calender</h2>
+                </SectionHeader>
+                <SectionBody name={"Calender"}>
+                    WHATEVER CONTENT IS
+                </SectionBody>
+            </Section>
+        </div>
+    )
   }
 }
