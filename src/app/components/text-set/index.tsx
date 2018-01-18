614edf2dea2263b0d2cfdadc578169978ef82d75
boydrotgans <boyd@lustlab.net>
2018-01-18 15:57:31 +0100

    Merge branch 'master' of https://bitbucket.org/rndrnl/openrndr-website
    # Conflicts:
    #	src/app/containers/sandbox.tsx

---
+++ src/app/components/text-set/index.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+import { TextBlock as TextBlockType } from "../../../types";
+import TextBlock from "../text-block";
+
+const Container = styled.div`
+  background: grey;
+  //border: solid 2px black;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
+interface Props {
+  data: TextBlockType[];
+}
+
+export default (props: Props) => {
+  return (
+    <Container>
+      {props.data.map(cb => {
+        return <TextBlock data={cb} />;
+      })}
+    </Container>
+  );
+};
