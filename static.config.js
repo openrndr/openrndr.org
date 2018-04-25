e9f72072d7c3e6982922a77ade6e8738fc2f8502
Gábor Kerekes <krks.gbr@gmail.com>
2018-04-25 13:50:15 +0200

    Disable JS interop 😠

--- static.config.js
+++ static.config.js
@@ -52,28 +52,18 @@ export default {
   webpack: (config, { defaultLoaders }) => {
     // Add .ts and .tsx extension to resolver
     config.resolve.extensions.push(".ts", ".tsx");
-
     // Add TypeScript Path Mappings (from tsconfig via webpack.config.js)
     // to react-statics alias resolution
     config.resolve.alias = typescriptWebpackPaths.resolve.alias;
-
-    // We replace the existing JS rule with one, that allows us to use
-    // both TypeScript and JavaScript interchangeably
     config.module.rules = [
       {
         oneOf: [
           {
-            test: /\.(js|jsx|ts|tsx)$/,
+            test: /\.(ts|tsx)$/,
             exclude: defaultLoaders.jsLoader.exclude, // as std jsLoader exclude
             use: [
               {
-                loader: "babel-loader"
-              },
-              {
-                loader: require.resolve("ts-loader"),
-                options: {
-                  transpileOnly: true
-                }
+                loader: require.resolve("ts-loader")
               }
             ]
           },
