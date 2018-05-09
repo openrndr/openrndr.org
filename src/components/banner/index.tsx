1ab8ef72284a1d85291c0822af807bf16ee5f23f
amirhouieh <amir@amirs-iMac-2.local>
2018-05-09 14:30:24 +0200

    fix banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -2,16 +2,37 @@ import * as React from "react";
 
 import "./style.css";
 import { Project as ProjectType } from "../../types/index";
-import { GalleryMediaItem } from "../gallery-mediaItem/index";
+import { ImageX } from "../image-x/index";
+import { BackgroundGif } from "../background-gif/index";
+import { Video } from "../video/index";
 
 interface IProps {
   data: ProjectType;
 }
 
 export const Banner: React.SFC<IProps> = ({ data }) => {
+  const thumbnail = data.media[0];
+
   return (
     <section className={"banner"}>
-      <GalleryMediaItem thumbnail={data.media[0]} autoPlay={true} />
+      {(function() {
+        switch (thumbnail.itemType) {
+          case "image":
+            return <ImageX data={thumbnail} fit={"fill"} />;
+          case "gif":
+            return <BackgroundGif data={thumbnail} />;
+          case "video":
+            return (
+              <Video
+                data={thumbnail}
+                width={`100%`}
+                height={`100%`}
+                playing={true}
+                controls={false}
+              />
+            );
+        }
+      })()}
     </section>
   );
 };
