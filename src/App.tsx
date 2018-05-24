e94ee01df854a886c7405e1ccea408e96bd336ce
amirhouieh <amir@amirs-iMac-2.local>
2018-05-24 12:33:21 +0200

    add fonts back

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { hot } from "react-hot-loader";
 
 import Routes from "react-static-routes";
 
-// import "@ibm/type/css/ibm-type.min.css";
+import "@ibm/type/css/ibm-type.min.css";
 import "./app.css";
 import { GridLines } from "./components/grid-lines/index";
 import { IDatoSiteData } from "./types/site";
