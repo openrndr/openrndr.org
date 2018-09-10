2c26ed677a4ac27a95d043b1bddf9b072112b1df
Gábor Kerekes <krks.gbr@gmail.com>
2018-09-10 11:45:02 +0200

    Hardcode event type order and titles in calendar

--- src/components/section-calendar/index.tsx
+++ src/components/section-calendar/index.tsx
@@ -5,21 +5,27 @@ import "./style.css";
 import { EventBlock } from "../event-block/index";
 import { Calendar } from "../../types";
 
+const expectedTypes: [string, string][] = [
+  ["event", "events"],
+  ["workshop", "workshops"],
+  ["exhibition", "exhibitions"]
+];
+
 export const SectionCalendar: React.SFC<{ data: Calendar }> = ({ data }) => {
   const groups = groupBy(data.events, "eventType");
-  return (
-    <section className={`calendar-section xx-x-x`}>
-      {Object.keys(groups)
-        .filter(groupKey => groups[groupKey].length > 0)
-        .map(groupKey => (
-          <div key={`event-group-${groupKey}`} className={"event-group"}>
-            <h3>{groupKey === "event" ? "events" : groupKey}</h3>
-            <br />
-            {groups[groupKey].map((event, i) => (
-              <EventBlock key={`event-${i}`} event={event} />
-            ))}
-          </div>
-        ))}
-    </section>
-  );
+  const elements = expectedTypes
+    .filter(([key]) => groups[key] && groups[key].length > 0)
+    .map(([key, title]) => {
+      const items = groups[key];
+      return (
+        <div key={`event-group-${key}`} className={"event-group"}>
+          <h3>{title}</h3>
+          <br />
+          {items.map((event, i) => (
+            <EventBlock key={`event-${i}`} event={event} />
+          ))}
+        </div>
+      );
+    });
+  return <section className={`calendar-section xx-x-x`}>{elements}</section>;
 };
